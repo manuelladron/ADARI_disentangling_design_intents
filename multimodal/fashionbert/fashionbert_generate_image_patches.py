c91691c40109f5621f30a85923e31b307a64b143
Manuel Ladron de Guevara <55259926+manuelladron@users.noreply.github.com>
2020-12-09 21:28:51 -0500

    Update fashionbert_generate_image_patches.py

--- multimodal/fashionbert/fashionbert_generate_image_patches.py
+++ multimodal/fashionbert/fashionbert_generate_image_patches.py
@@ -21,6 +21,7 @@ IMG_PATH = "/home/ubuntu/ADARI"
 PAIRS_PATH = "/home/ubuntu/ADARI_furniture_pairs.json"
 #IMG_PATH = "/home/alex/CMU/777/ADARI/v2/full"
 #PAIRS_PATH = "/home/alex/CMU/777/ADARI/ADARI_furniture_pairs.json"
+ENCODER_CNN_PATH = "resnet_9_v4.1.pt"
 
 def open_json(path):
     f = open(path) 
@@ -32,17 +33,14 @@ def save_json(file_path, data):
     out_file = open(file_path, "w")
     json.dump(data, out_file)
     out_file.close()
-
-
-# In[ ]:
-
-
-#dataset = MultiModalBertDataset(IMG_PATH, PAIRS_PATH, device=device)
-dataset = FashionBertRandomPatchesDataset(IMG_PATH, PAIRS_PATH, device=device)
+    
+    
+dataset = MultiModalBertDataset(IMG_PATH, PAIRS_PATH, ENCODER_CNN_PATH, device=device)
+# dataset = FashionBertRandomPatchesDataset(IMG_PATH, PAIRS_PATH, device=device)
 
 dataloader = DataLoader(dataset, batch_size=64, shuffle=False, drop_last=False)
 
-df = pd.DataFrame(columns=["patches", "input_ids", "is_paired", "attention_mask"])
+df = pd.DataFrame(columns=["patches", "input_ids", "is_paired", "attention_mask", "img_name", "patch_positions"])
 for j, (patches, input_ids, is_paired, attention_mask, img_name, patch_positions) in enumerate(dataloader):
     for i in range(patches.shape[0]):
         df = df.append(
@@ -60,9 +58,5 @@ for j, (patches, input_ids, is_paired, attention_mask, img_name, patch_positions
         print(f"At batch # {j}")
     
 
-
-# In[ ]:
-
-
-df.to_pickle("./preprocessed_patches.pkl")
+df.to_pickle("./preprocessed_normalpatches_img_adj_ft_resnet.pkl")
 
